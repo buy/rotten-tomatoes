d6cdfa069044678c061d8ce6b8a4ebbe9cdc9006
Chang Liu <cliu@groupon.com>
2015-10-25 15:41:12 -0500

    Initial Commit

---
+++ rotten-tomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rotten-tomatoes
+//
+//  Created by Chang Liu on 10/25/15.
+//  Copyright © 2015 Chang Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rotten-tomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rotten-tomatoes
+//
+//  Created by Chang Liu on 10/25/15.
+//  Copyright © 2015 Chang Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
